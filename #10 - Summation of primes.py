8c1990a6bb39dedf846d235411fdf7edcb39c34e
ekar07 <edkarwacki@gmail.com>
2015-05-15 00:08:58 -0500

    13 update
    more work on 13

--- #10 - Summation of primes.py
+++ #10 - Summation of primes.py
@@ -2,20 +2,30 @@
 #The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 #Find the sum of all the primes below two million.
 #Written by: Ed Karwacki
-
-x = 2
-count = 1
-sum = 2
-for x in range (2,2000000):
-        x += 1
-        print x
-        n=2
+import math
+primesum = 2
+n=2000000
+nums = []
+for x in range (0,n):
+        nums.append(x)
+square = int(math.sqrt(n))
+for x in range (2,square):
+        i = 2
         isPrime = True
-        for n in range (2, x-1):
-                if isPrime == True:
-                        if x % n == 0:
-                                isPrime = False
+        for i in range (2,x):
+                if x%i == 0:
+                     isPrime = False
+                i += 1
         if isPrime == True:
-                sum += x
-                
-print "sum is ",sum
+                for num in range (x+1,n):
+                        if nums[num] % x == 0:
+                                #print nums[num]
+                                del nums[num]
+                                
+
+
+        x += 1
+
+print "done step 2"
+primesum = sum(nums)
+print "sum is ",primesum
